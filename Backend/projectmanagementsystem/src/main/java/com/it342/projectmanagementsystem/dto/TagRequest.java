7370aa0de8263ed12ae71cb3e7fec934cc2109e8
Anzyyy <foxtrot091503@gmail.com>
2025-04-21 20:29:21 +0800

    Added TaggingSystem for Appointments
    - added CRUD for tagging system

--- Backend/projectmanagementsystem/src/main/java/com/it342/projectmanagementsystem/dto/TagRequest.java
+++ Backend/projectmanagementsystem/src/main/java/com/it342/projectmanagementsystem/dto/TagRequest.java
@@ -1,9 +1,23 @@
 package com.it342.projectmanagementsystem.dto;
 
-import lombok.Data;
-
-@Data
 public class TagRequest {
     private String name;
     private String color;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 } 
