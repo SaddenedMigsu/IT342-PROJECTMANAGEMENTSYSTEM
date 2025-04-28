66f95adcbaf4d65ae0fb5dd71c0d4c5be19fa701
Anzyyy <foxtrot091503@gmail.com>
2025-04-29 00:26:56 +0800

    Updated SecurityConfig and CorsConfig
    - added frontend urls

--- Backend/projectmanagementsystem/src/main/java/com/it342/projectmanagementsystem/config/SecurityConfig.java
+++ Backend/projectmanagementsystem/src/main/java/com/it342/projectmanagementsystem/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:[*]"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:[*]", "https://it342pms.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Auth-Token"));
         configuration.setExposedHeaders(Arrays.asList("X-Auth-Token"));
